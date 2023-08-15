cf78dc977371b6f08b58158e896d185180cd7244
DaveyStacks <david.c@renderinghouse.com>
2023-08-15 16:58:43 -0400

    Header with Next Nav wrappers. Ready for page navigation

--- app/components/ui/header/Header.jsx
+++ app/components/ui/header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link'
 import classes from './Header.module.css';
 import { RollingText } from '@/app/components/ui/textDisplays/rollingText/RollingText'
 
@@ -6,23 +7,29 @@ export const Header = () => (
   <nav>
     <div className={classes.header}>
       <div className={classes.partialBorderDiv} />
-      <div className={classes.menuItems}>
-        <div className={classes.menuItem}>
-          <div className={classes.itemText}>
-            <RollingText displayText={'ABOUT'} fontSize={'2rem'} minFontSize={'0.5rem'}></RollingText>
+        <div className={classes.menuItems}>
+          <div className={classes.menuItem}>
+            <div className={classes.itemText}>
+              <Link href={'/'}>
+                  <RollingText displayText={'ABOUT'} fontSize={'1rem'} minFontSize={'0.5rem'}></RollingText>
+              </Link>
+            </div>
+          </div>
+          <div className={classes.menuItem}>
+            <div className={classes.itemText}>
+              <Link href={'/'}>
+                <RollingText displayText={'HOME'} fontSize={'1rem'} minFontSize={'0.5rem'}></RollingText>
+              </Link>
+            </div>
+          </div>
+          <div className={classes.menuItem}>
+            <div className={classes.itemText}>
+              <Link href={'/'}>
+                <RollingText displayText={'EXPERIENCE'} fontSize={'1rem'} minFontSize={'0.5rem'}></RollingText>
+              </Link>
+            </div>
           </div>
         </div>
-        <div className={classes.menuItem}>
-          <div className={classes.itemText}>
-            <RollingText displayText={'HOME'} fontSize={'2rem'} minFontSize={'0.5rem'}></RollingText>
-          </div>
-        </div>
-        <div className={classes.menuItem}>
-          <div className={classes.itemText}>
-            <RollingText displayText={'EXPERIENCE'} fontSize={'2rem'} minFontSize={'0.5rem'}></RollingText>
-          </div>
-        </div>
-      </div>
     </div>
   </nav>
 );
