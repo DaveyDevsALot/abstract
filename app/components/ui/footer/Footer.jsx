71a92a46ed33ad54222eb2c1d0016b101b126fbd
DaveyStacks <david.c@renderinghouse.com>
2023-08-15 17:45:24 -0400

    Perfecting the base layout. It is the backbone of the app!

--- app/components/ui/footer/Footer.jsx
+++ app/components/ui/footer/Footer.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 import classes from './Footer.module.css';
 import { RollingText } from '../../ui/textDisplays/rollingText/RollingText'
+import Link from 'next/link'
 
 export const Footer = () => (
   <footer>
     <div className={classes.footer}>
       <div className={classes.partialBorderDiv} />
-        <div className={classes.textContainer}>
-          <div className={classes.topLine}>
-            <RollingText displayText={'SOFTWARE MAKES EVERYTHING BETTER'} fontSize={'2.5rem'} minFontSize={'1rem'}></RollingText>
+      <div className={classes.menuItems}>
+        <div className={classes.menuItem}>
+          <div className={classes.itemText}>
+            <Link href={'/'}>
+              <RollingText displayText={'SOFTWARE MAKES EVERYTHING BETTER'} fontSize={'2.5rem'} minFontSize={'1rem'}></RollingText>
+            </Link>
           </div>
-            <RollingText displayText={'SO DOES SMILING'} fontSize={'2rem'} minFontSize={'0.5rem'}></RollingText>
         </div>
+        <div className={classes.menuItem}>
+          <div className={classes.itemText}>
+            <Link href={'/'}>
+              <RollingText displayText={'SO DOES SMILING'} fontSize={'2rem'} minFontSize={'0.5rem'}></RollingText>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 );
-
-Footer.propTypes = {
-
-};
-
-Footer.defaultProps = {
-};
