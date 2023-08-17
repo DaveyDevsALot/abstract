ef0e2af23cfccc72125f7fb38ce252f20a163fe2
DaveyStacks <david.c@renderinghouse.com>
2023-08-16 20:23:17 -0400

    Completes more interactive landing page. Also reworks styles for ease of layout

--- app/components/ui/textDisplays/rollingText/RollingText.jsx
+++ app/components/ui/textDisplays/rollingText/RollingText.jsx
@@ -3,35 +3,44 @@ import React, { useLayoutEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import SplitType from 'split-type'
 import classes from './RollingText.module.css'
-export const RollingText = ({ displayText, fontSize, minFontSize = '0.5rem' }) => {
+export const RollingText = ({
+  displayText,
+  fontSize,
+  minFontSize = '0.5rem',
+}) => {
   const ref = useRef()
 
   useLayoutEffect(() => {
-    const repeatCount = 20;
-    const split = new SplitType("span", { type: "chars" });
-    const tl = gsap.timeline({ paused: true });
+    const repeatCount = 15
+    const split = new SplitType('span', { type: 'chars' })
+    const tl = gsap.timeline({ paused: true })
 
     split.chars.forEach((obj, i) => {
-      let txt = obj.innerText;
-      let clone = `<div class="cloneText" style="position:absolute;top:0"> ${txt} </div>`;
-      obj.innerHTML = `<div class="originalText"> ${txt} </div>${clone}`;
+      let txt = obj.innerText
+      let clone = `<div class="cloneText" style="position:absolute;top:0"> ${txt} </div>`
+      obj.innerHTML = `<div class="originalText"> ${txt} </div>${clone}`
       gsap.set(obj.childNodes[1], {
-        yPercent: i % 2 === 0 ? -100 : 100
-      });
+        yPercent: i % 2 === 0 ? -100 : 100,
+      })
       let tween = gsap.to(obj.childNodes, {
         repeat: repeatCount,
-        ease: "none",
-        yPercent: i % 2 === 0 ? "+=100" : "-=100"
-      });
-      tl.add(tween, 0);
-    });
-    gsap.to(tl, { progress: 1, duration: 8, ease: "power4.inOut" });
-
-  }, []);
+        ease: 'none',
+        yPercent: i % 2 === 0 ? '+=100' : '-=100',
+      })
+      tl.add(tween, 0)
+    })
+    gsap.to(tl, { progress: 1, duration: 8, ease: 'power4.inOut' })
+  }, [])
 
   return (
-    <div className={classes.stage} ref={ref} style={{fontSize: `clamp(${minFontSize}, -0.875rem + 5vw, ${fontSize})`}}>
+    <div
+      className={classes.stage}
+      ref={ref}
+      style={{
+        fontSize: `clamp(${minFontSize}, -0.875rem + 5vw, ${fontSize})`,
+      }}
+    >
       <span className={classes.cloneText}>{displayText}</span>
     </div>
-  );
+  )
 }
--- app/views/main/Main.jsx
+++ app/views/main/Main.jsx
@@ -1,49 +1,81 @@
-import classes from './Main.module.css'
+'use client'
+import { useLayoutEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { gsap } from 'gsap'
 import { RollingText } from '../../components/ui/textDisplays/rollingText/RollingText'
+import classes from './Main.module.css'
+import SplitType from 'split-type'
+
 export const Main = () => {
+  const [el, setEl] = useState(null)
+  const [el2, setEl2] = useState(null)
+
+  useLayoutEffect(() => {
+    if (!el || !el2) return
+    gsap.from(el, {
+      opacity: 0,
+      duration: 8,
+    })
+    gsap.to(el2, {
+      opacity: 0,
+      duration: 8,
+    })
+  }, [el, el2])
+
   return (
-    <section className={classes.homeMain}>
-      <div className={classes.homeItem}>
-        <div className={classes.mainImage}>
-          <Image
-            width={200}
-            height={400}
-            src="/main/darker_b_&_w.png"
-            alt="Black and White Image of David R Coulombe"
-          />
-        </div>
-        <div className={classes.mainImage2}>
-          <Image
-            width={200}
-            height={400}
-            src="/main/David_Site_Impressionist.png"
-            alt="Black and White Image of David R Coulombe"
-          />
-        </div>
-      </div>
-      <div className={classes.mainText}>
-        <div className={classes.menuItem}>
-          <div className={classes.itemText}>
-            <Link href={'/'}>
-              <RollingText
-                displayText={'DAVID COULOMBE'}
-                fontSize={'3.5rem'}
-                minFontSize={'2rem'}
-              ></RollingText>
-            </Link>
+    <section>
+      <div className={classes.homeMain}>
+        <div className={classes.homeItem}>
+          <div className={classes.images}>
+            <div className={classes.mainImage}>
+              <Image
+                width={200}
+                height={200}
+                src="/main/darker_b_&_w.png"
+                alt="Black and White Image of David R Coulombe"
+              />
+            </div>
+            <div className={classes.mainImage2} id="fadeIn" ref={setEl}>
+              <Image
+                width={200}
+                height={200}
+                src="/main/David_Site_Impressionist.png"
+                alt="Black and White Image of David R Coulombe"
+              />
+            </div>
+            <div className={classes.mainImage2} id="fadeOut" ref={setEl2}>
+              <Image
+                width={200}
+                height={200}
+                src="/main/darker_b_&_w.png"
+                alt="Black and White Image of David R Coulombe"
+              />
+            </div>
           </div>
         </div>
-        <div className={classes.menuItem}>
-          <div className={classes.itemText}>
-            <Link href={'/'}>
-              <RollingText
-                displayText={'SENIOR REACT ENGINEER'}
-                fontSize={'2rem'}
-                minFontSize={'1rem'}
-              ></RollingText>
-            </Link>
+        <div className={classes.mainText}>
+          <div className={classes.menuItem}>
+            <div className={classes.itemText}>
+              <Link href={'/'}>
+                <RollingText
+                  displayText={'DAVID COULOMBE'}
+                  fontSize={'3.5rem'}
+                  minFontSize={'2rem'}
+                ></RollingText>
+              </Link>
+            </div>
+          </div>
+          <div className={classes.menuItem}>
+            <div className={classes.itemText}>
+              <Link href={'/'}>
+                <RollingText
+                  displayText={'SENIOR REACT ENGINEER'}
+                  fontSize={'2rem'}
+                  minFontSize={'1rem'}
+                ></RollingText>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
